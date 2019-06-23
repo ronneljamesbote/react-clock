1afa0215dff197d753a3d0f106026b8abe55edef
ronneljamesdev <ronneljamesdev@pop-os.localdomain>
2019-06-23 16:38:22 +0800

    Add proptypes and defaultprops

--- src/components/controlPanel/controlPanel.js
+++ src/components/controlPanel/controlPanel.js
@@ -1,9 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
+import exact from "prop-types-exact";
 import React from "react";
 import { SwitchButton } from "../ui";
 import styles from "./styles.module.scss";
 
-function controlPanel({ checked, className = "", onChange, value }) {
+controlPanel.propTypes = exact({
+  checked: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.bool.isRequired,
+  className: PropTypes.string
+});
+
+controlPanel.defaultProps = {
+  className: ""
+};
+
+function controlPanel({ checked, className, onChange, value }) {
   const classes = [styles.controlPanel, className];
 
   return (
